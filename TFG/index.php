f755a5512cf43157a22b9a24145ac5544c7c29bb
Mario Yáñez Borrajo <Mario Yez Borrajo>
2016-04-04 00:23:24 +0200

    Inicio

---
+++ TFG/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+require_once("layouts/main/header.html");
+?>
+</head>
+<body>
+<?php
+require_once("layouts/main/menu.html");
+require_once("layouts/main/body.html");
+?>
+</body>
+</html>
